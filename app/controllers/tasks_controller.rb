b99404eaaba80867b16f9247ebc4668b715abed9
Alex Kutzke <alex@kutzke.com.br>
2018-02-07 15:15:26 -0200

    Corrige redirecionamento do tasks#update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to root_url, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
